8d3127420337d60f188401f3fb11a32cf8a48279
Ben Youcheng <youcheng.ben@sap.com>
2013-11-09 02:10:26 +0800

    add image processing tools and an image for test

--- src/tools/Constant.java
+++ src/tools/Constant.java
@@ -14,4 +14,13 @@ public final class Constant {
 	public static final String gDbUserName = "root";
 	public static final String gDbPwd = "root";
 	
+	/*image*/
+	public static final String IMAGE_ACTUAL_PATH = "./WebContent/images/";
+	public static final String IMAGE_ACCESS_PATH = "/images/";
+	public static final String IMAGE_TYPE_GIF = "gif";
+	public static final String IMAGE_TYPE_JPG = "jpg";
+	public static final String IMAGE_TYPE_JPEG = "jpeg";
+	public static final String IMAGE_TYPE_BMP = "bmp";  
+	public static final String IMAGE_TYPE_PNG = "png";
+    public static final String IMAGE_TYPE_PSD = "psd";   // photoshop
 }
