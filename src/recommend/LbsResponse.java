248ccabdd9b5b3ba0dddbf83f93887e02b2e7c91
Evan Dai <daiwei_wow@126.com>
2013-11-10 18:21:41 +0800

    finish store recommend model

---
+++ src/recommend/LbsResponse.java
@@ -0,0 +1,77 @@
+package recommend;
+
+import java.util.List;
+
+/**
+ * @Title: LbResponse
+ * @Description:
+ * @Company: ZhongHe
+ * @author dai
+ * @date 2013年11月10日
+ */
+public class LbsResponse {
+    private int status;
+    private String message;
+    private String total;
+    private List results;
+
+    /**
+     * @return the status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status
+     *            the status to set
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message
+     *            the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the total
+     */
+    public String getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total
+     *            the total to set
+     */
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    /**
+     * @return the results
+     */
+    public List getResults() {
+        return results;
+    }
+
+    /**
+     * @param results
+     *            the results to set
+     */
+    public void setResults(List results) {
+        this.results = results;
+    }
+}
---
+++ src/recommend/Location.java
@@ -0,0 +1,43 @@
+package recommend;
+
+/**
+ * @Title: Location
+ * @Description:
+ * @Company: ZhongHe
+ * @author dai
+ * @date 2013年11月10日
+ */
+public class Location {
+    private String lat;
+    private String lng;
+
+    /**
+     * @return the lat
+     */
+    public String getLat() {
+        return lat;
+    }
+
+    /**
+     * @param lat
+     *            the lat to set
+     */
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    /**
+     * @return the lng
+     */
+    public String getLng() {
+        return lng;
+    }
+
+    /**
+     * @param lng
+     *            the lng to set
+     */
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+}
