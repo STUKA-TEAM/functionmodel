a9c9d77cc5ad76c90adeff3517efe67e0c58f72e
Ben Youcheng <youcheng.ben@sap.com>
2013-11-11 10:52:36 +0800

    save latest changes

---
+++ src/lottery/LotteryPrize.java
@@ -0,0 +1,86 @@
+package lottery;
+
+/**
+ * @Title: LotteryPrize
+ * @Description:
+ * @Company: ZhongHe
+ * @author ben
+ * @date 2013年11月11日
+ */
+public class LotteryPrize {
+	private String prizeName;
+	private String prizeContent;
+	private int luckyNum;
+	private double luckyPercent;
+	
+	/**
+	 * @category constructor()
+	 */
+	public LotteryPrize(String prizeName, String prizeContent, int luckyNum, 
+			double luckyPercent){
+		this.prizeName = prizeName;
+		this.prizeContent = prizeContent;
+		this.luckyNum = luckyNum;
+		this.luckyPercent = luckyPercent;
+	}
+	
+	/**
+	 * @return the prizeName
+	 */
+	public String getPrizeName() {
+		return prizeName;
+	}
+	/**
+	 * @param prizeName the prizeName to set
+	 */
+	public void setPrizeName(String prizeNameString) {
+		this.prizeName = prizeNameString;
+	}
+	/**
+	 * @return the prizeContent
+	 */
+	public String getPrizeContent() {
+		return prizeContent;
+	}
+	/**
+	 * @param prizeContent the prizeContent to set
+	 */
+	public void setPrizeContent(String prizeContent) {
+		this.prizeContent = prizeContent;
+	}
+	/**
+	 * @return the luckyNum
+	 */
+	public int getLuckyNum() {
+		return luckyNum;
+	}
+	/**
+	 * @param luckyNum the luckyNum to set
+	 */
+	public void setLuckyNum(int luckyNum) {
+		this.luckyNum = luckyNum;
+	}
+	/**
+	 * @return the luckyPercent
+	 */
+	public double getLuckyPercent() {
+		return luckyPercent;
+	}
+	/**
+	 * @param luckyPercent the luckyPercent to set
+	 */
+	public void setLuckyPercent(double luckyPercent) {
+		this.luckyPercent = luckyPercent;
+	}	
+	
+/*	public static void main(String[] args){
+		LotteryPrize lp = new LotteryPrize("特等奖", "iphone5s 一部", 3, 0.0001);
+		LotteryPrize lp2 = new LotteryPrize("一等奖", "香港三日游旅游券1张", 30, 0.05);
+		List<LotteryPrize> list = new ArrayList<LotteryPrize>();
+		list.add(lp);
+		list.add(lp2);
+		Gson gson = new Gson();
+		String jsonRepresentation = gson.toJson(list);
+        System.out.println(jsonRepresentation);
+	}*/
+}
---
+++ src/lottery/NewLotteryActivityServlet.java
@@ -0,0 +1,102 @@
+package lottery;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import javax.servlet.http.Part;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import tools.DataBaseUtil;
+import tools.ImageUtil;
+
+/**
+ * Servlet implementation class NewLotteryActivityServlet
+ */
+@WebServlet("/NewLotteryActivityServlet")
+@MultipartConfig
+public class NewLotteryActivityServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewLotteryActivityServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		
+		/*normal input*/
+		String lotteryName = request.getParameter("LotteryName");
+		String lotterySummary = request.getParameter("LotterySummary");
+		String startDate = request.getParameter("StartDate");
+		String endDate = request.getParameter("EndDate");
+		
+		/*file input*/
+		Part lotteryPicture = request.getPart("LotteryPicture");
+		String picturePath = null;
+		if(lotteryPicture != null){
+			picturePath = request.getSession().getServletContext().getRealPath("");
+		    picturePath = ImageUtil.scaleFill(lotteryPicture.getInputStream(), picturePath, 400, 400);
+		}
+		
+		
+		/*json input*/
+		//simulate json input
+		LotteryPrize lp = new LotteryPrize("特等奖", "iphone5s 一部", 3, 0.0001);
+		LotteryPrize lp2 = new LotteryPrize("一等奖", "香港三日游旅游券1张", 30, 0.05);
+		List<LotteryPrize> list = new ArrayList<LotteryPrize>();
+		list.add(lp);
+		list.add(lp2);
+		Gson gson = new Gson();
+		String json = gson.toJson(list);
+		//json styles 
+		//[{"prizeName":"特等奖","prizeContent":"iphone5s 一部","luckyNum":3,
+		//"luckyPercent":1.0E-4},{"prizeName":"一等奖","prizeContent":"香港三日游旅游券1张",
+		//"luckyNum":30,"luckyPercent":0.05}]
+		List<LotteryPrize> lpList = gson.fromJson(json, new TypeToken<ArrayList
+				<LotteryPrize>>() {}.getType());
+		System.out.println(lpList.size());
+		
+/*		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter printWriter = response.getWriter();
+		printWriter.print( "收到表单数据："+"</br>"
+				            +"LotteryName："+ lotteryName + "</br>" 
+				            +"LotterySummary：" + lotterySummary + "</br>"
+				            +"StartDate:" + startDate + "</br>"
+				            +"EndDate:" + endDate + "</br>"
+				            +"BackgroundPath:" + picturePath + "</br>"
+				         );*/
+		
+/*		DataBaseUtil dataBaseUtil = DataBaseUtil.init();
+		dataBaseUtil.SqlExec("INSERT INTO lottery_activity"
+				+ " VALUES (default,'" + lotteryName + "','" + lotterySummary +
+				"','" + picturePath +"','"+ startDate + "','" + endDate +"',3,0)");*/
+	}
+
+}
